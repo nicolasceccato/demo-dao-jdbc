6d66e610ce56578979ec83018a0f218a26fe4155
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-08 10:32:33 -0300

    Adicionando método delete na classe SellerDaoJDBC

--- demo-dao-jdbc/src/main/java/Application/Program.java
+++ demo-dao-jdbc/src/main/java/Application/Program.java
@@ -7,10 +7,11 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -41,5 +42,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.println("Enter id for delet test");
+        int id = scanner.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        scanner.close();
     }
 }
