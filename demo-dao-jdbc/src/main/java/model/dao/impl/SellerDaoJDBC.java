ec21de9591fea2758fc4264b3c6f4ca9b839440d
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-08 09:14:09 -0300

    Adicionando método findAll na classe SellerDaoJDBC

--- demo-dao-jdbc/src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -84,7 +84,33 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department " + "ON seller.DepartmentId = department.Id " + "ORDER BY Id");
+
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
@@ -92,7 +118,7 @@ public class SellerDaoJDBC implements SellerDao {
         PreparedStatement st = null;
         ResultSet rs = null;
         try {
-            st = conn.prepareStatement("SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department " + "ON seller.DepartmentId = department.Id " + "WHERE  department.Id = ?");
+            st = conn.prepareStatement("SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department " + "ON seller.DepartmentId = department.Id " + "WHERE  department.Id = ? " + "ORDER BY Name");
 
             st.setInt(1, department.getId());
             rs = st.executeQuery();
